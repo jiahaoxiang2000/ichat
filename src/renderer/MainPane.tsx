04c221ab1fb5de72070809167147bd3365f0724b
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2025-02-22 23:14:47 +0800

    feat: integrate react-resizable for adjustable input box height in MainPane

--- src/renderer/MainPane.tsx
+++ src/renderer/MainPane.tsx
@@ -6,12 +6,20 @@ import MessageList from './components/MessageList'
 import { drawerWidth } from './Sidebar'
 import Header from './components/Header'
 import { sidebarVisibleAtom } from './stores/atoms'
+import { Resizable } from 'react-resizable'
+import { useState } from 'react'
+import 'react-resizable/css/styles.css'
 
 interface Props { }
 
 export default function MainPane(props: Props) {
     const currentSession = useAtomValue(atoms.currentSessionAtom)
     const sidebarVisible = useAtomValue(sidebarVisibleAtom)
+    const [inputBoxHeight, setInputBoxHeight] = useState(200)
+
+    const onResize = (e: React.SyntheticEvent, { size }: { size: { width: number; height: number } }) => {
+        setInputBoxHeight(size.height)
+    }
 
     return (
         <Box
@@ -22,10 +30,25 @@ export default function MainPane(props: Props) {
                 transition: 'margin-left 0.2s ease-in-out',
             }}
         >
-            <div className="flex flex-col h-full">
+            <div className="flex flex-col h-full relative">
                 <Header />
-                <MessageList />
-                <InputBox currentSessionId={currentSession.id} currentSessionType={currentSession.type || 'chat'} />
+                <div className="flex-grow overflow-hidden">
+                    <MessageList />
+                </div>
+                <Resizable
+                    height={inputBoxHeight}
+                    width={Infinity}
+                    onResize={onResize}
+                    resizeHandles={['n']}
+                    handle={<div className="absolute top-0 w-full h-2 cursor-row-resize hover:bg-gray-200/50" />}
+                >
+                    <div style={{ height: inputBoxHeight }}>
+                        <InputBox
+                            currentSessionId={currentSession.id}
+                            currentSessionType={currentSession.type || 'chat'}
+                        />
+                    </div>
+                </Resizable>
             </div>
         </Box>
     )
